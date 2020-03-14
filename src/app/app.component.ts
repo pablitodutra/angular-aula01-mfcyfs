a4ddcaebb94309b6366ad182c13390869125d220
pablitodutra <pablo.dutra@gmail.com>
2020-03-14 14:06:18 -0300

    1 file modified

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+// Decorador para transformar essa classe em um componete
+@Component({
+  selector : 'app-main', // seletor HTML
+  templateUrl : './app.component.html', // HTML do componente
+  styleUrls : [] // Estilos que serão carregados à página
+})
+
+export class AppComponent {
+
+}
