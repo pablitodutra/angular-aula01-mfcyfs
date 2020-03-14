60901fff9e8c61451f87701b529fd83f1f80e10a
pablitodutra <pablo.dutra@gmail.com>
2020-03-14 14:34:36 -0300

    1 file added

---
+++ src/app/meucomponente1/meucomponente1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector : 'meu-componente1',
+  templateUrl : 'meu-componente1.component.html',
+  styleUrls: []
+})
+export class MeuComponente1Component{
+
+}
