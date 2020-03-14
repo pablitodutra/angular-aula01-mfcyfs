72644bfb2c7f2682690aa8abf3b46c78602ff01f
pablitodutra <pablo.dutra@gmail.com>
2020-03-14 15:30:43 -0300

    1 file added

---
+++ src/app/func1/func1.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations : [], //Componentes do módulo
+  exports : [], // compomentes visíveis a outros módulos
+  imports : [CommonModule], // Quais módulos serão importados aqui
+})
+export class Func1Module{}
