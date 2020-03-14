a57c7dc9e4c5ea8a2988deebd188944ad4f6a802
pablitodutra <pablo.dutra@gmail.com>
2020-03-14 16:40:15 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
 import {MeuComponente1Component} from './meucomponente1/meucomponente1.component';
 import {Func1Module} from './func1/func1.module';
 
+
 @NgModule({
-  imports : [BrowserModule, Func1Module], // outros módulos usados
+  imports : [BrowserModule, Func1Module, FormsModule], // outros módulos usados
   exports : [], // tornar visível um componente, pipe...
   providers : [], // conjunto de serviços que estarão disponíveis para este módulo
   declarations : [AppComponent,MeuComponente1Component], // conjunto de componentes do módulo
