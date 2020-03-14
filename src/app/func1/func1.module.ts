5d1a71b15147aa2217379221cc622c15061c9fb5
pablitodutra <pablo.dutra@gmail.com>
2020-03-14 15:41:18 -0300

    1 file modified, 2 added

--- src/app/func1/func1.module.ts
+++ src/app/func1/func1.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule} from '@angular/common';
 import { NgModule } from '@angular/core';
+import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
-  declarations : [], //Componentes do módulo
+  declarations : [Comp1Component], //Componentes do módulo
   exports : [], // compomentes visíveis a outros módulos
   imports : [CommonModule], // Quais módulos serão importados aqui
 })
