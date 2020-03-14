2db1cbfc9abfde1b8387c0be13ea372ebd0d5c2b
pablitodutra <pablo.dutra@gmail.com>
2020-03-14 14:27:08 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 // Módulo necessário para que a aplicação possa ser desenhada em um navegador Web
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {AppComponent} from './app.component';
 
 @NgModule({
   imports : [BrowserModule], // outros módulos usados
   exports : [], // tornar visível um componente, pipe...
   providers : [], // conjunto de serviços que estarão disponíveis para este módulo
-  declarations : [], // conjunto de componentes do módulo
-  bootstrap : [] // é o conjunto de componentes pelo qual a aplicação web iniciará execução
+  declarations : [AppComponent], // conjunto de componentes do módulo
+  bootstrap : [AppComponent] // é o conjunto de componentes pelo qual a aplicação web iniciará execução
 })
 
 export class AppModule {
