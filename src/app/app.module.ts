82141f2f98a329a09610a1cbb2e22bcfe2cb465b
pablitodutra <pablo.dutra@gmail.com>
2020-03-14 14:15:09 -0300

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+// Módulo necessário para que a aplicação possa ser desenhada em um navegador Web
+import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+
 @NgModule({
-  imports : [], // outros módulos usados
+  imports : [BrowserModule], // outros módulos usados
   exports : [], // tornar visível um componente, pipe...
   providers : [], // conjunto de serviços que estarão disponíveis para este módulo
   declarations : [], // conjunto de componentes do módulo
